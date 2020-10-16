fe4ebb3d868393034d2d07d40266410c093ac6e7
OussamaDev <oussama.lahmidi@icloud.com>
2020-10-16 22:02:00 +0100

    Added new project module

--- src/main/webapp/app/new-template/modules/projet.tsx
+++ src/main/webapp/app/new-template/modules/projet.tsx
@@ -4,17 +4,26 @@ import EmailSidebar from '../components/email/emailSidebar';
 import ProjetActions from './projet/projetActions';
 import ProjetSearch from './projet/projetSearch';
 import ProjetContent from './projet/projetContent';
+import ProjetCreate from './projet/projetCreate';
+import { getEntities, filterEntities } from 'app/entities/projet/projet.reducer';
+
 import { connect } from 'react-redux';
 import { IRootState } from 'app/shared/reducers';
-import { getEntities } from 'app/entities/projet/projet.reducer';
 import { getSortState, Translate } from 'react-jhipster';
 import { ITEMS_PER_PAGE } from 'app/shared/util/pagination.constants';
 import { Button, Row, Col } from 'reactstrap';
-import { Mail, Plus, Upload, Download } from 'react-feather';
+import { Mail, Plus, Upload, Download, Link, Edit, Trash } from 'react-feather';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { apiUrl, ACTION_TYPES } from 'app/entities/projet/projet.reducer';
+import Export from '../../shared/Repport/export';
+import Import from '../../shared/Repport/import';
+import NavbarSearch from '../components/search/Search';
 
 const Projet = props => {
   const [paginationState, setPaginationState] = useState(getSortState(props.location, ITEMS_PER_PAGE));
-
+  const [modalOpen, setModalOpen] = useState(false);
+  const [entityModel, setEntityModel] = useState(null);
+  const [importExportOpen, setImportExportOpen] = useState(null);
   const getAllEntities = () => {
     props.getEntities(paginationState.activePage - 1, paginationState.itemsPerPage, `${paginationState.sort},${paginationState.order}`);
   };
@@ -43,6 +52,32 @@ const Projet = props => {
       ...paginationState,
       activePage: currentPage
     });
+  const handleClose = () => {
+    setEntityModel(null);
+    setModalOpen(false);
+  };
+
+  const editEntity = () => {
+    setEntityModel(props.selected);
+    console.log(props.selected, entityModel);
+    //  if (entreprise.image) props.getImage(entreprise.image.id);
+
+    // setIsNew(false);
+    setModalOpen(true);
+  };
+
+  const filter = e => {
+    console.log(e);
+    props.filterEntities(e);
+    // console.log(entrepriseList);
+  };
+
+  const confirmDelete = (id: number) => {
+    if (confirm('Voulez vous supprimer le projet avec ID ' + id)) {
+      props.deleteEntity(id);
+      // projetList = [];
+    }
+  };
 
   const { projetList, match, loading, totalItems } = props;
 
@@ -56,32 +91,60 @@ const Projet = props => {
             <div className="email-app-list-mails p-0">
               <Row className="p-2">
                 <Col md="12">
-                  <Button className="my-2 btn-raised" color="danger" block>
+                  <Button className="my-2 btn-raised" onClick={() => setModalOpen(true)} color="danger" block>
+                    {' '}
                     <Plus size={18} className="mr-1" /> <Translate contentKey="entity.action.create">Entreprises</Translate>
                   </Button>
                 </Col>
                 <Col md="6">
-                  <Button className="my-2 btn-raised" color="danger" block>
+                  <Button onClick={() => setImportExportOpen('IMP')} className="my-2 btn-raised" color="danger" block>
+                    {' '}
                     <Upload size={18} className="mr-1" />
                     Importer
                   </Button>
                 </Col>
                 <Col md="6">
-                  <Button className="my-2 btn-raised" color="danger" block>
+                  <Button onClick={() => setImportExportOpen('EXP')} className="my-2 btn-raised" color="danger" block>
+                    {' '}
                     <Download size={18} className="mr-1" /> <Translate contentKey="entity.action.download">Entreprises</Translate>
                   </Button>
                 </Col>
               </Row>
-              <ProjetSearch />
-              <div className="email-actions px-2 bg-white">
-                <ProjetActions />
+              <div className="email-search-box w-100 bg-white p-2">
+                <NavbarSearch search={filter} clear={props.getEntities} />
               </div>
+              {props.selected && (
+                <div className="email-actions px-2 bg-white">
+                  <Row>
+                    <Col xs="8">
+                      {/* <Button className="float-left email-action-icon"> */}
+                      <Edit className="float-left email-action-icon" size={18} color="#212529" onClick={() => editEntity()} />
+                      {/* </Button> */}
+                      {/* <Button className="float-left email-action-icon"> */}
+                      <Trash
+                        className="float-left email-action-icon"
+                        size={18}
+                        color="#212529"
+                        onClick={() => confirmDelete(props.selected.id)}
+                      />
+                      {/* </Button> */}
+                    </Col>
+                  </Row>
+                </div>
+              )}
               <ProjetList list={projetList} />
             </div>
             <ProjetContent />
           </div>
         </div>
+        <ProjetCreate modalOpen={modalOpen} handleClose={handleClose} entityModel={entityModel} />
       </div>
+      <Modal isOpen={importExportOpen !== null} toggle={() => setImportExportOpen(null)} size="md">
+        <ModalHeader toggle={() => setImportExportOpen(null)}>{importExportOpen === 'IMP' ? 'Importer' : 'Exporter'}</ModalHeader>
+        <ModalBody>
+          {importExportOpen === 'IMP' ? <Import apiUrl={apiUrl} /> : <Export apiUrl={apiUrl} action={ACTION_TYPES.REPPORT} />}
+        </ModalBody>
+      </Modal>
     </Fragment>
   );
 };
@@ -89,11 +152,13 @@ const Projet = props => {
 const mapStateToProps = ({ projet }: IRootState) => ({
   projetList: projet.entities,
   loading: projet.loading,
-  totalItems: projet.totalItems
+  totalItems: projet.totalItems,
+  selected: projet.entity
 });
 
 const mapDispatchToProps = {
-  getEntities
+  getEntities,
+  filterEntities
 };
 
 type StateProps = ReturnType<typeof mapStateToProps>;
